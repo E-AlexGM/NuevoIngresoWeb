414dded9eba691047ed69be030b41151e2f02477
E-AlexGM <eagranadinomendoza@gmail.com>
2026-04-13 22:08:39 -0600

    Add: .gitignore

---
+++ src/js/boundary/prueba.js
@@ -0,0 +1,17 @@
+class Prueba {
+    constructor() {
+        console.log("Hola desde Prueba");
+    }
+
+    saludar() {
+        console.log("¡Hola, mundo!");
+    }
+
+    despedirse() {
+        console.log("¡Adiós, mundo!");
+    }
+}
+
+var prueba = new Prueba();
+prueba.saludar();
+prueba.despedirse();
