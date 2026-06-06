f69c9d59e039ae27ef328b3158320e1690f4bb73
E-AlexGM <eagranadinomendoza@gmail.com>
2026-06-05 18:41:06 -0600

    remove: eliminados métodos no utilizados en las los dao y pruebas de sistema

--- test/js/control/unitario/prueba_jornada_dao_test.js
+++ test/js/control/unitario/prueba_jornada_dao_test.js
@@ -19,23 +19,7 @@ suite("PruebaJornadaDAO Unit Test", function () {
             stubFetch = null;
         }
     });
-
-    suite("findRange()", function () {
-        test("Debe delegar la llamada a _findRange con los parámetros recibidos", function () {
-            const mockPromise = Promise.resolve("datos_findRange");
-            stubFetch = sinon.stub(cut, "_findRange").returns(mockPromise);
-
-            const firstParam = 0;
-            const maxParam = 10;
-
-            const resultado = cut.findRange(firstParam, maxParam);
-
-            chai.assert.equal(resultado, mockPromise);
-            chai.assert.isTrue(stubFetch.calledOnce);
-            chai.assert.isTrue(stubFetch.calledWithExactly(firstParam, maxParam));
-        });
-    });
-
+    
     suite("create()", function () {
         test("Debe delegar la llamada a _create con el objeto recibido", function () {
             const mockPromise = Promise.resolve("datos_create");
@@ -49,38 +33,6 @@ suite("PruebaJornadaDAO Unit Test", function () {
             chai.assert.equal(resultado, mockPromise);
             chai.assert.isTrue(stubFetch.calledOnce);
             chai.assert.isTrue(stubFetch.calledWithExactly(pruebaJornada));
-        });
-    });
-
-    suite("findById()", function () {
-        test("Debe delegar la llamada a _findById con el id recibido", function () {
-            const mockPromise = Promise.resolve("datos_findById");
-            stubFetch = sinon.stub(cut, "_findById").returns(mockPromise);
-
-            const idParam = "123";
-
-            const resultado = cut.findById(idParam);
-
-            chai.assert.equal(resultado, mockPromise);
-            chai.assert.isTrue(stubFetch.calledOnce);
-            chai.assert.isTrue(stubFetch.calledWithExactly(idParam));
-        });
-    });
-
-    suite("update()", function () {
-        test("Debe delegar la llamada a _update con los parámetros recibidos", function () {
-            const mockPromise = Promise.resolve("datos_update");
-            stubFetch = sinon.stub(cut, "_update").returns(mockPromise);
-
-            const idParam = "123";
-            const pruebaJornada = new PruebaJornada();
-            pruebaJornada.idJornada = 2;
-
-            const resultado = cut.update(idParam, pruebaJornada);
-
-            chai.assert.equal(resultado, mockPromise);
-            chai.assert.isTrue(stubFetch.calledOnce);
-            chai.assert.isTrue(stubFetch.calledWithExactly(idParam, pruebaJornada));
         });
     });
 
